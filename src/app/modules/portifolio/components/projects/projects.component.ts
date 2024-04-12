680ca79050ca485240dbc3b3a46877265cb7ad9c
Diego Feijó Chagas <diego.chagas@unimedmaceio.local>
2024-04-12 11:38:11 -0300

    addition dialog box

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,21 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+
+//Interface
 import { IProjects } from '../../interface/IProjects.interfaces';
+
+//Material
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+
+//Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+//Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports:[MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProject = signal<IProjects[]>([
     {
     src: 'assets/img/projects/rocketseat.png',
     alt: 'projeto de portfólio web, em html5 e css3',
     title: 'Portifólio',
     width: '100px',
-    height: '51px',
+    height: '70px',
     description: 'Meu primeiro projeto envolvento programação web a partir do curso da Rocketseat',
     links:[{
       href: 'https://diegofchaggas.github.io/Project-Rocketseat/',
@@ -23,4 +34,10 @@ export class ProjectsComponent {
     }],
     }
   ])
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
